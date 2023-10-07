d7ebb753c6f723f02a872c3be85ccb5536e84754
StepanVulkan <119614126+StepanVulkan@users.noreply.github.com>
2023-10-08 01:44:16 +0300

    Add files via upload

---
+++ sound/Increase_decrease_the_volume/decrease.py
@@ -0,0 +1,24 @@
+import wave
+import struct
+
+
+def decrease(frame, count):
+    frame //= count
+    if frame <= -32768:
+        return -32768
+    elif frame >= 32767:
+        return 32767
+    return frame
+
+
+increase_number = 100
+source = wave.open("in.wav", mode="rb")
+dest = wave.open("out_decrease.wav", mode="wb")
+dest.setparams(source.getparams())
+frames_count = source.getnframes()
+data = struct.unpack("<" + str(frames_count) + "h", source.readframes(frames_count))
+newdata = list(map(lambda frame: decrease(frame, increase_number), data))
+newframes = struct.pack("<" + str(len(newdata)) + "h", *newdata)
+dest.writeframes(newframes)
+source.close()
+dest.close()
